3bfdd6e95674a9737cd2693296522b853279bee6
Prasan Yapa <toprasanyapa@gmail.com>
2024-04-17 15:40:09 +0900

    Update biencoder.py
    Initializing the dual-encoder considering the source task utterance_encoder and target demonstrations ctx_encoder.

--- version-1/biencoder.py
+++ version-1/biencoder.py
@@ -1,7 +1,8 @@
+from typing import Tuple
+
 import torch
-from torch import nn
-import torch.nn.functional as F
 from torch import Tensor as T
+from torch import nn
 
 
 class BiEncoder(nn.Module):
@@ -57,6 +58,44 @@ class BiEncoder(nn.Module):
                 )
 
         return sequence_output, pooled_output, hidden_states
+
+    def forward(
+        self,
+        utterance_ids: T,
+        utterance_segments: T,
+        utterance_positions: T,
+        utterance_attn_mask: T,
+        context_ids: T,
+        ctx_segments: T,
+        ctx_attn_mask: T,
+        encoder_type: str = None,
+        representation_token_pos=0,
+    ) -> Tuple[T, T]:
+        utterance_encoder = (
+            self.utterance_model
+            if encoder_type is None or encoder_type == "classification"
+            else self.ctx_model
+        )
+        _q_seq, q_pooled_out, _q_hidden = self.get_representation(
+            utterance_encoder,
+            utterance_ids,
+            utterance_segments,
+            utterance_positions,
+            utterance_attn_mask,
+            self.fix_uttr_encoder,
+            representation_token_pos=representation_token_pos,
+        )
+
+        ctx_encoder = (
+            self.ctx_model
+            if encoder_type is None or encoder_type == "ctx"
+            else self.utterance_model
+        )
+        _ctx_seq, ctx_pooled_out, _ctx_hidden = self.get_representation(
+            ctx_encoder, context_ids, ctx_segments, ctx_attn_mask, self.fix_ctx_encoder
+        )
+
+        return q_pooled_out, ctx_pooled_out
 
 
 def dot_product_scores(uttr_vectors: T, ctx_vectors: T) -> T:
